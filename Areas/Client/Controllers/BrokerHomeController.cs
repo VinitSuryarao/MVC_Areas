b18e0194400537e5bcc47ac0f95a8f78b467f73d
Vinit Suryarao <suryarao.vinit@hotmail.com>
2021-07-24 19:48:27 +0530

    Add files via upload

---
+++ Areas/Client/Controllers/BrokerHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Area.Areas.Client.Controllers
+{
+    public class BrokerHomeController : Controller
+    {
+        // GET: Client/BrokerHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
